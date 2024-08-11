3248032ad30cbb48097aafc2e55800a31aa010bf
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-08-11 19:21:58 +0200

    Added an Angular route guard - v 0.0.4

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { OfferListComponent } from './offers/offer-list/offer-list.component';
 import { OfferDetailComponent } from './offers/offer-detail/offer-detail.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'offers', component: OfferListComponent},
-    {path: 'offers/:id', component: OfferDetailComponent},
-    {path: 'lists', component: ListsComponent},
-    {path: 'messages', component: MessagesComponent},
+    {
+        path:'',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'offers', component: OfferListComponent},
+            {path: 'offers/:id', component: OfferDetailComponent},
+            {path: 'lists', component: ListsComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
+    
     {path: '**', component: HomeComponent, pathMatch: 'full'} 
 ];
