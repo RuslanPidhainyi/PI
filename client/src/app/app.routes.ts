902983d3f5d71affc67516b311b15c88536a7b09
Ruslan <136593314+RuslanPidhainyi@users.noreply.github.com>
2024-08-11 18:48:02 +0200

    Added routes and added components  - v 0.0.1

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { OfferListComponent } from './offers/offer-list/offer-list.component';
+import { OfferDetailComponent } from './offers/offer-detail/offer-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'offers', component: OfferListComponent},
+    {path: 'offers/:id', component: OfferDetailComponent},
+    {path: 'lists', component: ListsComponent},
+    {path: 'messages', component: MessagesComponent},
+    {path: '**', component: HomeComponent, pathMatch: 'full'} 
+];
---
+++ client/src/app/offers/offer-detail/offer-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-detail.component.html',
+  styleUrl: './offer-detail.component.css'
+})
+export class OfferDetailComponent {
+
+}
